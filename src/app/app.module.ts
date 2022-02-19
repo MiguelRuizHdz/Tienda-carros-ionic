4d07abb881a79efb971f8fb098456d17eb09ef6a
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-02-18 20:08:25 -0600

    Almacenando en el Storage del dispositivo Carrito de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -17,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    IonicStorageModule.forRoot()
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,12 @@ export class CarritoService {
 
   items: any[] = [];
 
-  constructor(public alertController: AlertController) { }
+  constructor(public alertController: AlertController,
+    private platform: Platform ,
+    private storage: Storage) {
+      this.storage.create();
+    this.cargarStorage();
+    }
 
   async agregarCarrito( itemParametro: any ) {
     for( let item of this.items ) {
@@ -25,6 +31,43 @@ export class CarritoService {
     }
 
     this.items.push( itemParametro );
+    this.guardarStorage();
+  }
+
+  async cargarStorage(){
+
+    return new Promise( async (resolve, reject ) => {
+
+      if( this.platform.is('capacitor') ) {
+        // dispositivo
+        await this.storage.get('items').then( items => {
+          if (items) {
+            this.items = items;
+          }
+          resolve(true);
+        });
+      } else {
+        // computadora
+        if ( localStorage.getItem('items') ) {
+          // Existe items en el localStorage
+          this.items = JSON.parse( localStorage.getItem('items') );
+        }
+        resolve(true);
+      }
+    });
+
+  }
+
+  private guardarStorage() {
+
+    if( this.platform.is('capacitor')){
+      // dispositivo
+      this.storage.set('items', this.items);
+    } else {
+      // computadora
+      localStorage.setItem('items', JSON.stringify( this.items ) );
+    }
+
   }
 
 
